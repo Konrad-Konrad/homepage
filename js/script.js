a8e41d101000185770cc4b12a3f728fc29b73090
Konrad-Konrad <67336690+Konrad-Konrad@users.noreply.github.com>
2020-06-29 16:20:23 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+{
+    function HelloMessage() {
+        console.log("Hello my friend")
+    }
+    HelloMessage();
+    const HideAdvertising = () => {
+        const button = document.querySelector(".buttonHideAdvertising")
+        const reklama = document.querySelector(".reklama")
+        reklama.remove();
+    };
+
+    const toogleBackground = (zmienTlo) => {
+        const calaStrona = document.querySelector(".body__background")
+        calaStrona.classList.toggle("body__background-change")
+        if (zmienTlo.innerText === "Zmień tło na różowe") {
+            zmienTlo.innerText = "Zmień tło na szare";
+        } else
+            zmienTlo.innerText = "Zmień tło na różowe";
+    };
+
+    const zmienTlo = document.querySelector(".button2");
+    zmienTlo.addEventListener("click", toogleBackground);
+
+    const button = document.querySelector(".buttonHideAdvertising");
+    button.addEventListener("click", HideAdvertising);
+}
